aa1e235d784f84e64b738183027523627134098f
j0n-3kw3r3 <johnebong950@gmailcom>
2023-12-09 21:54:22 +0100

    8080

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 const authRoute = require("./Routes/auth");
 const eventRoute = require("./Routes/events");
 const materialRoute = require("./Routes/materials");
